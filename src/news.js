7a014ed1ad78669c15af012c3781a1b2addd03d6
Simon Cass <scass1991@gmail.com>
2019-03-12 12:09:22 +0000

    Renamed, added failing test for news

--- src/news.js
+++ src/news.js
@@ -1,20 +1,9 @@
-//https://www.w3schools.com/xml/dom_httprequest.asp
-"use strict"
-
-class News {
-  constructor () {
-    this.headlines = []
-  }
-
-  scrapeStories (story = new XMLHttpRequest()) {
-    story = story;
-    const my_key = config.MY_KEY
-    let url = `https://content.guardianapis.com/search?api-key=${my_key}`
-      request.open('GET', url)
-      request.onload = () => {
-        let info = JSON.parse(story.response)
-        this.headlines.push(info.response.results)
-    }
-    request.send()
-  }
-}
+describe('News', () => {
+  it('has a title and a url', () => {
+    var title = "This is the web title"
+    var url = "This is the web url"
+    var news = new News(title,url)
+    expect(news.title).toEqual(title)
+    expect(news.url).toEqual(url)
+   })
+ })
